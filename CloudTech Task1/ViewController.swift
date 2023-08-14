dda2ae72eaeee1379723dc92eff4c64e7f680965
Badal Aryal <badal.aryal@gmail.com>
2023-08-14 10:52:36 +0545

    Initial Commit

---
+++ CloudTech Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudTech Task1
+//
+//  Created by Badal  Aryal on 14/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
